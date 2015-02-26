3303bc4328b89ce830edb73345f85697af45842b
lin <dxswxpt@163.com>
2015-02-26 16:29:26 +0800

    backone

---
+++ js/models.js
@@ -0,0 +1,19 @@
+App.Models.Team = Backbone.Model.extend({
+	defaults : {
+		name : ""
+	},
+	
+	initialize : function() {
+        this.on('change:name',function(){    //绑定change事件，当数据改变时执行此回调函数
+            alert('changed');
+        });
+	},
+	
+	validate : function(attributes){
+		if (!!attributes && attributes.name === "teamX") {
+			return "Error!";
+		}
+	}
+	
+
+});
